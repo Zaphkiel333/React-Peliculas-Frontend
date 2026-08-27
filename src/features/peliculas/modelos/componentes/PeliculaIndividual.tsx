a4c03357236a4c3bbc4c194e8072025d47781e0d
Rafael Hernandez <rhhs333@gmail.com>
2026-08-26 22:14:06 -0600

    Componentes genericos, gift y mas

---
+++ src/features/peliculas/modelos/componentes/PeliculaIndividual.tsx
@@ -0,0 +1,21 @@
+import type Pelicula from "../pelicula.model";
+import styles from "./PeliculaIndividual.module.css";
+
+export default function PeliculaIndividual(props: PeliculaIndividualProps) {
+  const construirLink = () => `/pelicula/${props.pelicula.id}`;
+
+  return (
+    <div className={styles.div}>
+      <a href={construirLink()}>
+        <img alt="Poster" src={props.pelicula.poster} />
+      </a>
+      <p>
+        <a href={construirLink()}>{props.pelicula.titulo}</a>
+      </p>
+    </div>
+  );
+}
+
+interface PeliculaIndividualProps {
+  pelicula: Pelicula;
+}
