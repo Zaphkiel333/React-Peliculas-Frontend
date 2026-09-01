ce789c95c7b87a861f9128eb4475e14c1450e607
Rafael Hernandez <rhhs333@gmail.com>
2026-08-31 20:18:47 -0600

    Reglas de validacion

--- src/componentes/Boton.tsx
+++ src/componentes/Boton.tsx
@@ -1,6 +1,11 @@
 export default function Boton(props: BotonProps) {
   return (
-    <button type="button" className="btn btn-primary" onClick={props.onClick}>
+    <button
+      type={props.type ?? "button"}
+      className="btn btn-primary"
+      onClick={props.onClick}
+      disabled={props.disabled ?? false}
+    >
       {props.children}
     </button>
   );
@@ -8,5 +13,7 @@ export default function Boton(props: BotonProps) {
 
 interface BotonProps {
   children: React.ReactNode;
-  onClick(): void;
+  onClick?(): void;
+  type?: "button" | "submit" | "reset";
+  disabled?: boolean;
 }
--- src/features/generos/componentes/CrearGenero.tsx
+++ src/features/generos/componentes/CrearGenero.tsx
@@ -1,3 +1,58 @@
+import { NavLink } from "react-router";
+import Boton from "../../../componentes/Boton";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { primeraLetraMayuscula } from "../../../validaciones/Validaciones";
+
 export default function CrearGenero() {
-  return <h3>Crear Género</h3>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid, isSubmitting },
+  } = useForm<FormType>({
+    resolver: yupResolver(reglasDeValidacion),
+    mode: "onChange",
+  });
+
+  const onSubmit: SubmitHandler<FormType> = async (data) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(data);
+  };
+
+  return (
+    <>
+      <h3>Crear Género</h3>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-group">
+          <label htmlFor="nombre">Nombre</label>
+          <input
+            autoComplete="off"
+            className="form-control"
+            {...register("nombre")}
+          />
+          {errors.nombre && <p className="error">{errors.nombre.message}</p>}
+        </div>
+        <div className="mt-2">
+          <Boton type="submit" disabled={!isValid || isSubmitting}>
+            {isSubmitting ? "Enviando..." : "Enviar"}
+          </Boton>
+          <NavLink to="/generos" className="btn btn-secondary ms-2">
+            Cancelar
+          </NavLink>
+        </div>
+      </form>
+    </>
+  );
 }
+
+interface FormType {
+  nombre: string;
+}
+
+const reglasDeValidacion = yup.object({
+  nombre: yup
+    .string()
+    .required("El nombre es obligatorio")
+    .test(primeraLetraMayuscula()),
+});
